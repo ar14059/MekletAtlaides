ed70857ed299a6e6f119dbce6752c8d7d433ab9e
Artis Raudive <artisraudivelu@gmail.com>
2016-03-30 14:27:16 +0300

    Pievienoju koda rindu, kas maina simbolu kopu uz UTF-8, lai, ievadot reģistrācijas datus, tiktu atpazīti burti ar garumzīmēm.

--- base.php
+++ base.php
@@ -7,5 +7,6 @@ $dbuser = "root"; // the username that you created, or were given, to access you
 $dbpass = ""; // the password that you created, or were given, to access your database
  
 $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("MySQL Error: " . mysql_error());
+mysqli_set_charset($con, 'utf8');
 // mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
 ?>
