08c0dec2ba5fe6304859a2b0eeb7f10a8397b5bf
Artis Raudive <artisraudivelu@gmail.com>
2016-03-31 19:01:14 +0300

    Pārdēvēju failu 'par_mums.html' uz 'par_mums.php'.

---
+++ par_mums.php
@@ -0,0 +1,79 @@
+<?php include "base.php"; ?>
+
+<html>
+<head>
+    
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <link rel="stylesheet" type="text/css" href="css/par_mums.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <script type="text/javascript" src="scripts/jquery.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="scripts/jquery.js"></script>
+
+
+    <title>Webpage</title>
+</head>
+<body>
+
+    <section class="body-welcome">
+<!--         <div id="main-nav_include"></div> -->
+        <script> 
+        // $(function(){
+        //   $("#main-nav_include").load("main-nav.html"); 
+        // });
+        </script> 
+
+        <header class="main-nav">
+            <div class="header-right">
+                
+                <div id="navigation_include"></div>
+                <script> 
+                $(function(){
+                  $("#navigation_include").load("navigation.php"); 
+                });
+                </script> 
+                <a href="home.php"><button id="nav-right-button-back" class="button_nav"></button></a>
+            </div>
+        </header>
+
+        <section id="body-content">
+            <section class="par-mums">
+                <div id="par-mums_content">
+                    <fieldset class="content">
+                        <p class="subject-title">Par mums</p>
+                        <hr>
+                        <p class="paragraph">In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since. 'Whenever you feel like criticizing anyone,' he told me, 'just remember that all the people in this world haven't had the advantages that you've had. In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since. 'Whenever you feel like criticizing anyone,' he told me, 'just remember that all the people in this world haven't had the advantages that you've had. In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since. 'Whenever you feel like criticizing anyone,' he told me, 'just remember that all the people in this world haven't had the advantages that you've had.</p>
+                        <p class="paragraph">​Aizvadītā gada nogalē VAS "​Tiesu namu aģentūra​" izdotajai ​Latvijas Universitātes Juridiskās fakultātes profesoru Valentijas Liholajas un Ulda Karstiņa ​grāmatai "Krimināllikuma komentāri. Pirmā daļa (I–VIII nodaļa)" nu iznācis turpinājums ​- pie lasītājiem dodas "Krimināllikuma komentāri. Otrā daļa (IX–XVII nodaļa)". Šobrīd tiek izstrādāta jau komentāru 3.daļa. Aizvadītā gada nogalē VAS "​Tiesu namu aģentūra​" izdotajai ​Latvijas Universitātes Juridiskās fakultātes profesoru Valentijas Liholajas un Ulda Karstiņa ​grāmatai "Krimināllikuma komentāri. Pirmā daļa (I–VIII nodaļa)" nu iznācis turpinājums ​- pie lasītājiem dodas "Krimināllikuma komentāri. Otrā daļa (IX–XVII nodaļa)". Šobrīd tiek izstrādāta jau komentāru 3.daļa. </p>
+                        <p class="paragraph">​Aizvadītā gada nogalē VAS "​Tiesu namu aģentūra​" izdotajai ​Latvijas Universitātes Juridiskās fakultātes profesoru Valentijas Liholajas un Ulda Karstiņa ​grāmatai "Krimināllikuma komentāri. Pirmā daļa (I–VIII nodaļa)" nu iznācis turpinājums ​- pie lasītājiem dodas "Krimināllikuma komentāri. Otrā daļa (IX–XVII nodaļa)". Šobrīd tiek izstrādāta jau komentāru 3.daļa. Aizvadītā gada nogalē VAS "​Tiesu namu aģentūra​" izdotajai ​Latvijas Universitātes Juridiskās fakultātes profesoru Valentijas Liholajas un Ulda Karstiņa ​grāmatai "Krimināllikuma komentāri. Pirmā daļa (I–VIII nodaļa)" nu iznācis turpinājums ​- pie lasītājiem dodas "Krimināllikuma komentāri. Otrā daļa (IX–XVII nodaļa)". Šobrīd tiek izstrādāta jau komentāru 3.daļa. </p>
+
+
+                    </fieldset>
+                </div>
+            </section>
+        </section>
+
+
+        <div id="footer_include"></div>
+        <script> 
+        $(function(){
+          $("#footer_include").load("footer.html"); 
+        });
+        </script>
+
+    </section>
+    
+
+
+
+
+
+
+</body>
+
+
+
+</html>
+
