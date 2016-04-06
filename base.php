2d40d8b1152d849b0c4f8115ce349dc9633d8a38
Artis Raudive <artisraudivelu@gmail.com>
2016-04-06 22:24:52 +0300

    Pamainīju datubāzi no 'mekletatlaides' uz 'mekletatlaides_example'.

--- base.php
+++ base.php
@@ -2,7 +2,7 @@
 session_start();
  
 $dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
-$dbname = "mekletatlaides"; // the name of the database that you are going to use for this project
+$dbname = "mekletatlaides_example"; // the name of the database that you are going to use for this project
 $dbuser = "root"; // the username that you created, or were given, to access your database
 $dbpass = ""; // the password that you created, or were given, to access your database
  
