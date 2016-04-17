dc3548b605828687bc91f75ac43ee7f3f1a4df7d
Artis Raudive <artisraudivelu@gmail.com>
2016-04-17 23:27:45 +0300

    Pievienoju funkciju, kas nosaka, kādā web-brauzerī darbina mājaslapu.

--- base.php
+++ base.php
@@ -8,6 +8,31 @@ $dbpass = ""; // the password that you created, or were given, to access your da
  
 $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("MySQL Error: " . mysql_error());
 mysqli_set_charset($con, 'utf8');
+
+// $browser = get_browser(null, true);
+// echo $browser['browser'];
 // mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
 ?>
+
+<?php
+function get_browsername() {
+	if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE){
+		$browser = 'Microsoft Internet Explorer';
+	}elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE) {
+		$browser = 'Chrome';
+	}elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE) {
+		$browser = 'Firefox';
+	}elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== FALSE) {
+		$browser = 'Opera';
+	}elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== FALSE) {
+		$browser = 'Safari';
+	}else {
+		$browser = 'error';
+	}
+	return $browser;
+}
+$browser = get_browsername();
+?>
+
+
 
