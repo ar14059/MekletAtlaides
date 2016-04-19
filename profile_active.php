473f4793b64ca479629bfbe2d89d570c18f047c6
Artis Raudive <artisraudivelu@gmail.com>
2016-04-19 13:38:23 +0300

    Pievienoju kodu, kas aktivizē/deaktivizē konkrētu uzņēmumu.

--- profile_active.php
+++ profile_active.php
@@ -2,14 +2,33 @@
 <?php include 'functions.php'; ?>
 
 <?php
-	$u_id = $_GET['u_id'];
-	$type = $_GET['type'];
+	if($_GET['activation'] == 'user'){
+		$u_id = $_GET['u_id'];
+		$type = $_GET['type'];		
+	}else if($_GET['activation'] == 'company'){
+		$c_id = $_GET['c_id'];
+		$pieslegts = $_GET['pieslegts'];			
+	}
+	
 
-	if($type == 1){
-		mysqli_query($con, "UPDATE lietotajs SET Aktivs='0' WHERE id='$u_id'");
-		header("location: a_user_list.php");
-	}else{
-		mysqli_query($con, "UPDATE lietotajs SET Aktivs='1' WHERE id='$u_id'");
-		header("location: a_user_list.php");
+
+	if(!empty($u_id)){
+		if($type == 1){
+			mysqli_query($con, "UPDATE lietotajs SET Aktivs='0' WHERE id='$u_id'");
+			header("location: a_user_list.php");
+		}else if($type == 0){
+			mysqli_query($con, "UPDATE lietotajs SET Aktivs='1' WHERE id='$u_id'");
+			header("location: a_user_list.php");
+		}
+
+	}
+	else if(!empty($c_id)){
+		if($pieslegts == 1){
+			mysqli_query($con, "UPDATE uznemums SET Pieslegts='0' WHERE id='$c_id'");
+			header("location: a_company_list.php");
+		}else if ($pieslegts == 0){
+			mysqli_query($con, "UPDATE uznemums SET Pieslegts='1' WHERE id='$c_id'");
+			header("location: a_company_list.php");
+		}
 	}
 ?>
