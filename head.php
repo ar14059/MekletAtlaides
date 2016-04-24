6bf842a05338e3fbb656399f3742eb0396bad7d7
Artis Raudive <artisraudivelu@gmail.com>
2016-04-24 11:20:16 +0300

    Izveidoju failu 'head.php', ievietoju tajā meta, link datus, pievienoju šo failu visiem .php failiem, izņemot adminpaneļa failiem.

---
+++ head.php
@@ -0,0 +1,14 @@
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+
+    <?php
+        if($curr_location!="/MekletAtlaides/registracija.php"){
+    ?>
+    <link rel="stylesheet" type="text/css" href="css/main_nav.css">
+    <?php } ?>
+    <link rel="stylesheet" type="text/css" href="css/main_nav.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+  
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    
