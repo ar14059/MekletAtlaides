17ea2086db58377c31e44864f19bc2db918deef7
Artis Raudive <artisraudivelu@gmail.com>
2016-04-24 11:13:44 +0300

    Pievienoju 'navigation.php' navigācijas joslas kodu. Izveidoju main_nav.css failu, kas nodrošina navigācijas joslas izskatu failā 'navigation.php'

--- navigation.php
+++ navigation.php
@@ -1,19 +1,14 @@
 
-<?php include "base.php"; ?>
-
-
-
-    <script src="scripts/jquery.js"></script>
-
     <?php
-if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['EmailAddress']))
+if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Epasts']))
 {
+    if($_SESSION['Lietotaja_limenis']<2){
     ?>
 
         <div class="name_surname_div">
             <p id="name_surname" class="login_user_data">Lietotājs 
-                <code><?=$_SESSION['Name'];?></code> 
-                <code><?=$_SESSION['Surname'];?></code>.
+                <code><?=$_SESSION['Vards'];?></code> 
+                <code><?=$_SESSION['Uzvards'];?></code>.
             </p>
         </div>
         <div id="main-settings-container">
@@ -24,32 +19,42 @@ if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['EmailAddress']))
             </div>
         </div>
       
-     <?php
+    <?php
+    }else{
+        header("location: a_home.php"); 
+    }
 }
 elseif(!empty($_POST['email']) && !empty($_POST['password']))
 {
     $email_address = mysqli_real_escape_string($con, $_POST['email']);
     $password = md5(mysqli_real_escape_string($con, $_POST['password']));
      
-    $checklogin = mysqli_query($con ,"SELECT * FROM users WHERE EmailAddress = '".$email_address."' AND Password = '".$password."'");
+    $checklogin = mysqli_query($con ,"SELECT * FROM lietotajs WHERE Epasts = '".$email_address."' AND Parole = '".$password."'");
      
     if(mysqli_num_rows($checklogin) == 1)
     {
         $row = mysqli_fetch_array($checklogin);
         // $email = $row['EmailAddress'];
-        $name = $row['Name'];
-        $surname = $row['Surname'];
-         
-        $_SESSION['EmailAddress'] = $email_address;
-        $_SESSION['Name'] = $name;
-        $_SESSION['Surname'] = $surname;
-        $_SESSION['LoggedIn'] = 1;
-         
+        $active = $row['Aktivs'];
+        if($active==1){ 
+            $id = $row['ID'];
+            $_SESSION['ID'] = $id;
+            $name = $row['Vards'];
+            $surname = $row['Uzvards'];
+            $user_level = $row['Lietotaja_limenis'];     
+            $_SESSION['Epasts'] = $email_address;
+            $_SESSION['Vards'] = $name;
+            $_SESSION['Uzvards'] = $surname;
+            $_SESSION['LoggedIn'] = 1;
+            $_SESSION['Lietotaja_limenis'] = $user_level;  
+        }else{
+            // echo "<h1>Jūs nobloķēja administrators!</h1>";
+        }
         header("Refresh:0");
     }
     else
     {
-    	echo "<script type='text/javascript'>alert('Worked but not how expected);</script>";
+        // echo "<script type='text/javascript'>alert('Worked but not how expected);</script>";
         echo "<h1>Error</h1>";
         echo "<p>Sorry, your account could not be found. Please <a href=\"home.php\">click here to try again</a>.</p>";
     }
@@ -57,12 +62,18 @@ elseif(!empty($_POST['email']) && !empty($_POST['password']))
 else
 {
     ?>
-
+    <?php
+        if($curr_location!="/MekletAtlaides/home.php"){
+    ?>
+    <a href="home.php"><button id="nav-right-button-back" class="button_nav"></button></a>
+    <?php
+        }
+    ?>
     <button id="hidden_button_login" class="button_nav"></button>
     <div id="loginContainer">
         <button id="nav-right-button-login" class="button_nav"></button>
         <div id="loginBox">   
-            <form method="post" action="home.php" name="loginForm" id="loginForm">
+            <form method="post" action="<?php echo  $curr_location; ?>" name="loginForm" id="loginForm">
             <hr>  
                 <fieldset id="body">
                     <fieldset>
@@ -78,10 +89,16 @@ else
             </form>
         </div>            
     </div>
-    <a href="registracija.php"><button id="nav-right-button-register" class="button_nav"></button></a>
-     
+    <div id="registerContainer">
+        <button id="nav-right-button-register" class="button_nav"></button>
+        <div id="registerBox">   
+            <form method="post" action="home.php" name="registerForm" id="registerForm">
+                <?php
+                echo "<a href='check_user_level.php?u_level=0' id='msb_pircejs' class='rsb_p'>Pircējs</a>";
+                echo "<a href='check_user_level.php?u_level=1' id='msb_uznemums' class='rsb_p'>Uzņēmums</a>";
+                ?>
+        </div>            
+    </div>
    <?php
 }
 ?>
- 
-
